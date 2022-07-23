19deb2e32543b8e5c18a63cbc5de1946a652824b
jusong23 <jusong23@naver.com>
2022-07-23 14:36:44 +0900

    Initial Commit

---
+++ RisingCamp_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp_TableView
+//
+//  Created by 이주송 on 2022/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

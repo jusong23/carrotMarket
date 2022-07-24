c4ff29553366316c65680b70a490a8e72ed4f24b
jusong23 <jusong23@naver.com>
2022-07-24 11:50:03 +0900

    DongNeLife Scroll View

---
+++ RisingCamp_TableView/DongNeLifeTab/DongNeLifeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DongNeLifeViewController.swift
+//  RisingCamp_TableView
+//
+//  Created by 이주송 on 2022/07/24.
+//
+
+import UIKit
+
+class DongNeLifeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RisingCamp_TableView/componentSetting.swift
@@ -0,0 +1,8 @@
+//
+//  componentSetting.swift
+//  RisingCamp_TableView
+//
+//  Created by 이주송 on 2022/07/24.
+//
+
+import Foundation

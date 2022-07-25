b07d1fa76fb5138dc8b9b03ebc93d457feca9c4f
jusong23 <jusong23@naver.com>
2022-07-25 20:14:16 +0900

    Reset Point

---
+++ RisingCamp_TableView/Chatting/Cell/ChattingCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingCell.swift
+//  RisingCamp_TableView
+//
+//  Created by 이주송 on 2022/07/25.
+//
+
+import UIKit
+
+class ChattingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ RisingCamp_TableView/ChattingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingViewController.swift
+//  RisingCamp_TableView
+//
+//  Created by 이주송 on 2022/07/25.
+//
+
+import UIKit
+
+class ChattingViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
